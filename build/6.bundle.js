660d1812045f8760fcf306c5d93e0cde46f60e09
seo1im <seolim6492@gmail.com>
2020-10-19 18:01:55 +0900

    build

--- build/6.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{38:function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return u}));var c=e(0),o=e(1);function u(){var n=Object(o.e)().pathname;return Object(c.useEffect)((function(){window.scrollTo(0,0)}),[n]),null}}}]);
